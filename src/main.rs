d6cb271bd0a2ade06f2f09be39b95de842dbf456
Mitchell Grenier <mitchell@confurious.io>
2021-01-12 18:20:37 -0500

    Clean up

--- src/main.rs
+++ src/main.rs
@@ -6,15 +6,9 @@ use agent::Agent;
 use std::os::unix::net::{UnixListener};
 
 use rustica::rustica_client::{RusticaClient};
-use rustica::{  CertificateRequest,
-                CertificateResponse,
-                ChallengeRequest,
-                ChallengeResponse,
-            };
+use rustica::{CertificateRequest, ChallengeRequest};
 
-use rustica_sshkey::ssh::{CertType, Certificate, PublicKey as SSHPublicKey, PublicKeyKind as SSHPublicKeyKind};
-
-use rustica_sshkey::yubikey::{asn_cert_signer, ssh_cert_signer, provision, ssh_cert_fetch_pubkey};
+use rustica_sshkey::yubikey::{asn_cert_signer, ssh_cert_signer, ssh_cert_fetch_pubkey};
 
 use std::collections::HashMap;
 use yubikey_piv::key::{RetiredSlotId, SlotId};
@@ -35,7 +29,7 @@ impl agent::SSHAgentHandler for Handler {
     }
 
     fn identities(&mut self) -> Result<agent::Response, agent::error::Error> {
-        println!("Received a call for identities");
+        println!("Refreshing certificate from authority");
         let our_identity = agent::Identity {
             key_blob: self.identities[0].key_blob.clone(),
             key_comment: "Test".to_string()
@@ -44,14 +38,8 @@ impl agent::SSHAgentHandler for Handler {
     }
 
     fn sign_request(&mut self, pubkey: Vec<u8>, data: Vec<u8>, flags: u32) -> Result<agent::Response, agent::error::Error> {
-        println!("Received sign request for key: {}", hex::encode(pubkey));
-        println!("Flags are: {}", flags);
         let signature = ssh_cert_signer(&data, SlotId::Retired(RetiredSlotId::R13)).unwrap();
-
         let signature = (&signature[27..]).to_vec();
-
-        println!("Data for signing with req key: {}", hex::encode(&data));
-        println!("Signature: {}", hex::encode(&signature));
 
         let response = agent::Response::SignResponse {
             algo_name: String::from("ecdsa-sha2-nistp256"),
@@ -65,10 +53,7 @@ impl agent::SSHAgentHandler for Handler {
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {    
     println!("Starting Rustica Agent");
-    //let mut yk = yubikey_piv::YubiKey::open().unwrap();
     let user_key_slot = SlotId::Retired(RetiredSlotId::R13);
-
-    //provision(&mut yk, b"123456", user_key_slot).unwrap();
     let ssh_pubkey = ssh_cert_fetch_pubkey(user_key_slot).unwrap();
     
     let encoded_key = format!("{}", ssh_pubkey);
@@ -79,18 +64,18 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
 
     let mut client = RusticaClient::connect("http://[::1]:50051").await?;
     let response = client.challenge(request).await?;
-    //println!("RESPONSE={:?}", response);
 
     let response = response.into_inner();
     let challenge_signature = hex::encode(asn_cert_signer(&hex::decode(&response.challenge).unwrap(), user_key_slot).unwrap());
     let request = tonic::Request::new(CertificateRequest {
         pubkey: encoded_key.to_string(),
         cert_type: 1,
+        key_id: String::from("JITC:obelisk@exclave"),
         challenge_time: response.time,
         critical_options: HashMap::new(),
         extensions: HashMap::new(),
         servers: vec!["atheris".to_string()],
-        users: vec!["obelisk".to_string()],
+        principals: vec!["obelisk".to_string()],
         valid_before: 0xFFFFFFFFFFFFFFFF,
         valid_after: 0x0,
         challenge: response.challenge,
@@ -99,11 +84,7 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
 
     let mut client = RusticaClient::connect("http://[::1]:50051").await?;
     let response = client.certificate(request).await?;
-    // println!("RESPONSE={:?}", response);
     let response = response.into_inner();
-
-    let parsed_certification = Certificate::from_string(&response.certificate).unwrap();
-    println!("{:#}", &parsed_certification);
     
     let cert: Vec<&str> = response.certificate.split(" ").collect();
     let cert = agent::Identity {
